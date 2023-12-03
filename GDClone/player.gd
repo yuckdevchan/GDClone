694fade02f634c280eb21917b966644a9c46654e
bigMonkeyMan2 <117944090+bigMonkeyMan2@users.noreply.github.com>
2023-12-03 13:19:24 +0000

    a

--- GDClone/player.gd
+++ GDClone/player.gd
@@ -3,11 +3,22 @@ const GRAVITY = 500
 @export var speed = 200
 
 var VELOCITY = 0
+var fall = false
 
+func _on_area_2d_area_entered(area):
+	var fall = false
+
+func _on_area_2d_area_exited(area):
+	var fall = true
+	
 func _process(delta):
-	position.x += speed * delta
+	while fall == true:
+		VELOCITY -= GRAVITY * delta
 	position.y -= VELOCITY * delta
+	position.x += speed * delta
 	if Input.is_action_just_pressed("up"):
-		VELOCITY += 200
-	VELOCITY -= GRAVITY * delta
+		VELOCITY += 20
 	move_and_slide()
+
+
+
