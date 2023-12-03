f2b251b7903f689fc662eef15d9513a5ddf28abb
bigMonkeyMan2 <117944090+bigMonkeyMan2@users.noreply.github.com>
2023-12-03 13:05:09 +0000

    code

--- GDClone/player.gd
+++ GDClone/player.gd
@@ -1,5 +1,13 @@
 extends CharacterBody2D
+const GRAVITY = 500
+@export var speed = 200
 
-@export var speed = 100
+var VELOCITY = 0
+
 func _process(delta):
 	position.x += speed * delta
+	position.y -= VELOCITY * delta
+	if Input.is_action_just_pressed("up"):
+		VELOCITY += 200
+	VELOCITY -= GRAVITY * delta
+	move_and_slide()
