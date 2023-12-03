d6e98364c97ec9d31d9d579f83db2a2e9dc84f19
bigMonkeyMan2 <117944090+bigMonkeyMan2@users.noreply.github.com>
2023-12-03 12:52:21 +0000

    fix2

--- GDClone/player.gd
+++ GDClone/player.gd
@@ -2,7 +2,4 @@ extends CharacterBody2D
 
 @export var speed = 100
 func _process(delta):
-	if Input.is_action_pressed("left"):
-		position.x += speed * delta
-	elif Input.is_action_pressed("right"):
-		position.x += -speed * delta
+	position.x += speed * delta
